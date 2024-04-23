004b44c976baf3882f37454f6319125e728a06f3
Adriana Tarquino <atarquinocespedes@gmail.com>
2024-04-23 08:31:53 -0400

    Add: Folders

---
+++ lib/My reservations/reservation_list.dart
@@ -0,0 +1,15 @@
+
+class Reserva {
+  String nombre;
+  String estado;
+
+  Reserva(this.nombre, this.estado);
+}
+
+List<Reserva> reservas = [
+  Reserva('Reserva 1', 'Confirmada'),
+  Reserva('Reserva 2', 'Pendiente'),
+  Reserva('Reserva 3', 'Confirmada'),
+  Reserva('Reserva 4', 'Pendiente'),
+];
+
